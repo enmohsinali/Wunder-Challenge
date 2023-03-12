618f05a7b67ad5ea73448688b2bcc6f7358dc6a9
Mohsin Ali <enmohsinali@gmail.com>
2023-03-12 17:02:19 +0500

    Add test case using jest

---
+++ tests/checkout.test.ts
@@ -0,0 +1,40 @@
+import { Product } from '../src/Product';
+import { Checkout } from '../src/Checkout';
+import { Promotion } from '../discount/Promotion';
+
+const products: Product[] = [
+  { code: "001", name: "Curry Sauce", price: 1.95 },
+  { code: "002", name: "Pizza", price: 5.99 },
+  { code: "003", name: "Men’s T-Shirt", price: 25.0 }
+];
+
+const promotionalRules: Promotion[] = [
+  {
+    itemDiscounts: [
+      {
+        productCode: "002",
+        minItems: 2,
+        discountPrice: 3.99,
+      },
+    ],
+  },
+  {
+    percentDiscount: {
+      discount: 10,
+      threshold: 30,
+    },
+  },
+];
+
+describe('Checkout', () => {
+  describe('total', () => {
+    it('should return the correct total for Test', () => {
+      const co = new Checkout(promotionalRules, products);
+      co.scan('001');
+      co.scan('002');
+      co.scan('003');
+      const total = co.total;
+      expect(total).toEqual('29.65');
+    });
+  });
+});
