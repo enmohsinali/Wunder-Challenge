a578783885ba0b98238a7640b7aacafb93fc7f24
Mohsin Ali <enmohsinali@gmail.com>
2023-03-12 16:51:39 +0500

    Add Checkout class and check it through main.ts file

---
+++ src/Checkout.ts
@@ -0,0 +1,67 @@
+import { Promotion } from '../discount/Promotion';
+import { Product } from './Product';
+
+export class Checkout {
+    private items: Product[] = [];
+    private promotions: Promotion[] = [];
+    private products: Product[];
+
+    constructor(promotions: Promotion[], products: Product[] | [] = []) {
+        this.promotions = promotions;
+        this.products = products;
+    }
+
+
+    scan(code: string) {
+        const product = this.products.find((p) => p.code === code);
+        if (product) {
+            this.items.push(product);
+        } else {
+            console.log(`Product with code ${code} not found.`);
+        }
+    }
+    private applyItemDiscounts(): number {
+        let discountTotal = 0;
+
+        this.promotions.forEach((promotion) => {
+            if (promotion.itemDiscounts) {
+                promotion.itemDiscounts.forEach((discount) => {
+                    const matchingItems = this.items.filter(
+                        (item) => item.code === discount.productCode
+                    );
+                    if (matchingItems.length >= discount.minItems) {
+                        const totalDiscount = matchingItems.reduce(
+                            (acc, item) => acc + item.price,
+                            0
+                        ) - discount.discountPrice * matchingItems.length;
+                        discountTotal += totalDiscount;
+                    }
+                });
+            }
+        });
+
+        return discountTotal;
+    }
+
+    private applyPercentDiscount(total: number): number {
+        let discountTotal = 0;
+
+        this.promotions.forEach((promotion) => {
+            if (promotion.percentDiscount && total >= promotion.percentDiscount.threshold) {
+                discountTotal = total * (promotion.percentDiscount.discount / 100);
+            }
+        });
+
+        return discountTotal;
+    }
+
+    total() {
+        const total = this.items.reduce((acc, item) => acc + item.price, 0);
+
+        const itemDiscountTotal = this.applyItemDiscounts();
+        const percentDiscountTotal = this.applyPercentDiscount(total - itemDiscountTotal);
+
+        const finalTotal = (total - itemDiscountTotal - percentDiscountTotal);
+        return Math.max(finalTotal).toFixed(2);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,38 @@
 import { Product } from './Product';
 import { Promotion } from '../discount/Promotion';
+import { Checkout } from './Checkout';
 
 // Example usage:
 
 const products: Product[] = [
-    { code: "001", name: "Curry Sauce", price: 1.95 },
-    { code: "002", name: "Pizza", price: 5.99 },
-    { code: "003", name: "Men’s T-Shirt", price: 25.0 }
-  ];
-  
- // Define the promotional rules
- const promotionalRules: Promotion[] = [
-    {
-      itemDiscounts: [
-        {
-          productCode: "002",
-          minItems: 2,
-          discountPrice: 3.99,
-        },
-      ],
-    },
-    {
-      percentDiscount: {
-        discount: 10,
-        threshold: 30,
+  { code: "001", name: "Curry Sauce", price: 1.95 },
+  { code: "002", name: "Pizza", price: 5.99 },
+  { code: "003", name: "Men’s T-Shirt", price: 25.0 }
+];
+
+// Define the promotional rules
+const promotionalRules: Promotion[] = [
+  {
+    itemDiscounts: [
+      {
+        productCode: "002",
+        minItems: 2,
+        discountPrice: 3.99,
       },
+    ],
+  },
+  {
+    percentDiscount: {
+      discount: 10,
+      threshold: 30,
     },
-  ];
-  
-console.log(products,promotionalRules);
+  },
+];
+
+// Create a checkout object with the promotional rules
+const co = new Checkout(promotionalRules, products);
+co.scan("001");
+co.scan("002");
+co.scan("003");
+const total1 = co.total(); // 29.65
+console.log(total1)
